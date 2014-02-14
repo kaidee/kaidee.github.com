05dd5fe1254a78700ab68012cc91a598b5b9b67c
kaidee <lkaidee@gmail.com>
2014-02-14 20:56:53 +0800

    add alias file

---
+++ alias.sh
@@ -0,0 +1,28 @@
+alias ls="ls -w"
+alias ll="ls -al"
+alias la="ls -a"
+alias v="vim"
+alias b="brew"
+alias u="cd .."
+alias c="cd"
+alias o="open"
+alias mk="make"
+alias bi="brew install"
+alias e="exit"
+alias s='open -a "Sublime Text 2"'
+alias sub='open -a "Sublime Text 2"'
+alias mate='open -a "TextMate"'
+alias cr='open -a "Google Chrome"'
+
+alias py='python'
+alias ipy='ipython'
+alias ipn='ipython notebook'
+
+
+alias gf="git flow"
+alias gff="git flow feature"
+alias gfr="git flow release"
+alias gfh="git flow hotfix"
+
+alias gcl="git clone"
+alias gb="git blame"
